c84ddba5cfdc721aa5559d858237080ed38ae4ae
Martin Danielsson <martin@breakit.se>
2018-12-04 11:21:40 +0100

    day1

---
+++ day1/day1.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+import "bufio"
+import "io"
+import "os"
+import "strconv"
+
+func main() {
+	input, err := os.Open("./input")
+	if err != nil {
+	    fmt.Println("error reading file", err);
+	}
+	frequencies, err := Parse(input)
+	if err != nil {
+	    fmt.Println("error parsing file", err);
+	}
+	fmt.Println("frequency is", Sum(frequencies))
+}
+
+func Parse(r io.Reader) ([]int, error) {
+    scanner := bufio.NewScanner(r)
+    scanner.Split(bufio.ScanWords)
+    var result []int
+
+    for scanner.Scan() {
+        x, err := strconv.Atoi(scanner.Text())
+
+        if err != nil {
+            return result, err
+        }
+
+        result = append(result, x)
+    }
+
+    return result, scanner.Err()
+}
+
+func Sum(input []int) int {
+    sum := 0
+
+    for i := range input {
+        sum += input[i]
+    }
+
+    return sum
+}
