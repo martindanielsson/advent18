692f0d96ec2fd34bc45b372a727e421e7878ee2d
Martin Danielsson <martin@breakit.se>
2018-12-04 14:05:54 +0100

    day 2 part 1

---
+++ day2/part1/part1.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	input, err := os.Open("../input")
+
+	barcodes, err := Parse(input)
+
+	if err != nil {
+		fmt.Println("error reading file", err);
+	}
+
+	fmt.Println("barcode checksum", CountBarcodes(barcodes))
+}
+
+func Parse(r io.Reader) ([]string, error) {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanWords)
+	var result []string
+
+	for scanner.Scan() {
+		x := scanner.Text()
+		result = append(result, x)
+	}
+
+	return result, scanner.Err()
+}
+
+func CountBarcodes(barcodes []string) int {
+	two := 0
+	three := 0
+
+	for i := range barcodes {
+		hasTwo := false
+		hasThree := false
+		split := strings.Split(barcodes[i], "")
+
+		chars := make(map[string]int)
+
+		for s := range split {
+			chars[split[s]]++
+		}
+
+		fmt.Println(chars)
+
+		for c := range chars {
+			if chars[c] == 2 && !hasTwo {
+				hasTwo = true
+				two++;
+			} else if chars[c] == 3 && !hasThree {
+				hasThree = true
+				three++
+			}
+		}
+
+		fmt.Println(hasTwo, hasThree, two, three)
+	}
+
+	return two * three
+}
